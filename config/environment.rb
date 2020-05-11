ea2b983fc582c93cb0f15487dd1d34a6020e3555
LaurentBourdel <solartech971@gmail.com>
2020-05-11 15:45:01 -0400

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['solartech971'],
-    :password => ENV['Laurent29'],
+    :password => ENV['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'],
     :domain => 'https://www.solartech971.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
